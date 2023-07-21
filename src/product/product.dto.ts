658cb355e9e66508f7b29398013814878b8b58dc
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-07-21 15:16:42 -0300

    fix: add ApiProperty to DTO

--- src/product/product.dto.ts
+++ src/product/product.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
   @IsNumber()
+  @ApiProperty()
   price: number;
 }
