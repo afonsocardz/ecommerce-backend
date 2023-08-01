144c171ed46558968e639693eaca5599582ab8af
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-08-01 15:17:05 -0300

    fix: remove sccure email option

--- src/app.service.ts
+++ src/app.service.ts
@@ -18,7 +18,6 @@ export class AppService {
     const options: SMTPTransport.Options = {
       host: this.configService.get<string>('SMTP_HOST'),
       port: this.configService.get<number>('SMTP_PORT'),
-      secure: this.configService.get<boolean>('SMTP_SECURE'),
       auth: {
         user: this.configService.get<string>('SMTP_USER'),
         pass: this.configService.get<string>('SMTP_PASSWORD'),
