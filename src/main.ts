d0677616a880edfe80b983b66e985bc8083e2024
afonsocardz <afonso.desenvolvimento@gmail.com>
2023-08-04 18:05:08 -0300

    feat: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appService = app.get(AppService);
   app.setGlobalPrefix('api');
+
+  app.use(cookieParser());
 
   app.enableCors({
     origin: appService.getOrigin(),
